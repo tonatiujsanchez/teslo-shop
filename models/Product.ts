330c5faaf278b0b0472a60958a758f09a08dafe8
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2022-08-06 13:23:43 -0500

    Endpoints de productos, producto por slug y search, añadidos

---
+++ models/Product.ts
@@ -0,0 +1,42 @@
+import mongoose, { Schema, model, Model } from "mongoose";
+import { IProduct } from "../interfaces";
+
+const productSchema = new Schema({
+    description: { type: String, required: true },
+    images: [{ type: String, required: true }],
+    inStock: { type: Number, required: true, default: 0 },
+    price: { type: Number, required: true, default: 0 },
+    sizes: [{
+        type: String,
+        enum: {
+            values: ['XS','S','M','L','XL','XXL','XXXL'],
+            message: '{VALUE} no es un tamaño válido'
+        }
+    }],
+    slug: { type: String, required: true, unique: true },
+    tags: [{ type: String }],
+    title: { type: String, required: true },
+    type: {
+        type: String,
+        enum: {
+            values: ['shirts','pants','hoodies','hats'],
+            message: '{VALUE} no es un tipo válido'
+        }
+    },
+    gender: {
+        type: String,
+        enum: {
+            values: ['men','women','kid','unisex'],
+            message: '{VALUE} no es un género válido'
+        }
+    }
+},{
+    timestamps: true
+})
+
+productSchema.index({ title: 'text', tags: 'text' })
+
+
+const Product: Model<IProduct> = mongoose.models.Product || model('Product', productSchema)
+
+export default Product
